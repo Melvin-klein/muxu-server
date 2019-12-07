191443f0f6bc773d8b4e58af269db918acf4191e
Melvin <m.nargeot@archidvisor.com>
2019-12-07 13:12:39 +0100

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -1,22 +1,20 @@
 "use strict";
 
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var express = require('express');
-
-var app = express();
-
-/*interface ServerOptionsInterface {
-    port: number
-}*/
-
+var app = (0, _express["default"])();
 /**
  * @class Server
  */
+
 var Server =
 /*#__PURE__*/
 function () {
@@ -50,11 +48,17 @@ function () {
   }, {
     key: "run",
     value: function run(options) {
+      if (this.mainComponent === null) {
+        console.error('No main component has been plugged to the server. Did you forget to call the server "plug()" method ?');
+        console.error('Server can\'t start');
+        return;
+      }
+
       app.get('/*', function (req, res) {
         res.send('Hello World!');
       });
       app.listen(options.port, function () {
-        console.log("Server is now listening on port : ".concat(options.port));
+        console.log("[Muxu Server] - Server is now listening on port : ".concat(options.port));
       });
     }
   }]);
