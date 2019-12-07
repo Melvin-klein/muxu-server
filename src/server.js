b4f7df69fbb3a30073f8228a284cc1aede7812ec
Melvin <m.nargeot@archidvisor.com>
2019-12-07 18:40:17 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 // @flow
 
+import fs from 'fs';
 import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+
 import type {ServerOptionsInterface} from './interfaces/serverOptionsInterface';
 import type {$Application, $Request, $Response} from 'express';
 
 const app: $Application = express();
+const router: express$Router = express.Router();
 
 /**
  * @class Server
@@ -12,7 +16,10 @@ const app: $Application = express();
 class Server {
     mainComponent: number | null;
     app: $Application;
+    router: express$Router;
     expressServer: http$Server | null;
+    baseHtmlString: string;
+    assetsDirPath: string | null;
 
     /**
      * Server constructor
@@ -20,7 +27,33 @@ class Server {
     constructor() {
         this.mainComponent = null;
         this.expressServer = null;
+        this.assetsDirPath = null;
         this.app = app;
+        this.router = router;
+    }
+
+    /**
+     * Set the HTML file where main component will be rendered
+     *
+     * @param {string} path
+     */
+    html(path: string): void {
+        if (!fs.existsSync(path)) {
+            console.error(`[Muxu Server] - Unable to find base HTML file at : ${path}`);
+
+            return;
+        }
+
+        this.baseHtmlString = fs.readFileSync(path, 'utf8');
+    }
+
+    /**
+     * Change assets directory path
+     *
+     * @param {string} path
+     */
+    assetsDir(path: string): void {
+        this.assetsDirPath = path;
     }
 
     /**
@@ -40,6 +73,7 @@ class Server {
      */
     run(options: ServerOptionsInterface): Promise<void> {
         return new Promise((resolve, reject) => {
+            // Check Main Component
             if (this.mainComponent === null) {
                 console.error('[Muxu Server] - No main component has been plugged to the server. Did you forget to call the server "plug()" method ?');
                 console.error('[Muxu Server] - Server can\'t start');
@@ -47,10 +81,33 @@ class Server {
                 reject(new Error());
             }
 
+            // Check Base HTML
+            if (this.baseHtmlString === null) {
+                console.error('[Muxu Server] - No HTML has been provided to the server. Did you forget to call the server "html()" method ?');
+                console.error('[Muxu Server] - Server can\'t start');
+
+                reject(new Error());
+            }
+
             try {
-                this.app.get('/*', function(req: $Request, res: $Response) {
-                    res.send('Hello World!');
-                });
+                const serverRenderedContent = (req: $Request, res: $Response, next: express$NextFunction) => {
+                    const MainComponent = this.mainComponent;
+
+                    return res.send(
+                        this.baseHtmlString.replace(
+                            '<div id="root"></div>',
+                            `<div id="root">${ReactDOMServer.renderToString(<MainComponent />)}</div>`,
+                        ),
+                    );
+                };
+
+                router.use('^/$', serverRenderedContent);
+
+                if (this.assetsDirPath !== null) {
+                    router.use(express.static(this.assetsDirPath, {maxAge: '30d'}));
+                }
+
+                this.app.use(this.router);
 
                 this.expressServer = this.app.listen(options.port, function() {
                     console.log(`[Muxu Server] - Server is now listening on port : ${options.port}`);
