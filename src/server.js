fe6d21f76cbfe520a519836e8e5cf84245592d0a
Melvin <m.nargeot@archidvisor.com>
2019-12-07 13:13:35 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 // @flow
 
 import express from 'express';
-import type { ServerOptionsInterface } from './interfaces/serverOptionsInterface';
+import type {ServerOptionsInterface} from './interfaces/serverOptionsInterface';
+import type {$Application, $Request, $Response} from 'express';
 
-const app = express();
+const app: $Application = express();
 
 /**
  * @class Server
@@ -36,13 +37,13 @@ class Server {
      */
     run(options: ServerOptionsInterface): void {
         if (this.mainComponent === null) {
-            console.error("No main component has been plugged to the server. Did you forget to call the server 'plug()' method ?");
-            console.error("Server can't start");
+            console.error('No main component has been plugged to the server. Did you forget to call the server "plug()" method ?');
+            console.error('Server can\'t start');
 
             return;
         }
 
-        app.get('/*', function(req, res) {
+        app.get('/*', function(req: $Request, res: $Response) {
             res.send('Hello World!');
         });
 
