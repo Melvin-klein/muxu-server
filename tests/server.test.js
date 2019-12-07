3acc8a75a4159bb2511ad2a6f90f9e81e00dd885
Melvin <m.nargeot@archidvisor.com>
2019-12-07 14:52:18 +0100

    Update server.test.js

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,3 +1,30 @@
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+const Axios = require('axios');
+const Server = require('../dist/server');
+
+const TestComponent = () => {};
+
+test('Can plug a main component to the server', () => {
+    const server = new Server();
+
+    server.plug(TestComponent);
+
+    expect(server.mainComponent).not.toBeNull();
+});
+
+test('Server can listen on a port', done => {
+    const server = new Server();
+
+    server.plug(TestComponent);
+
+    server.run({
+        port: 5000,
+    }).then(() => {
+        Axios.get('http://localhost:5000')
+            .then(response => {
+                expect(response.status).toBe(200);
+                server.stop();
+
+                done();
+            })
+    });
 });
